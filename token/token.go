b082f5e92da546b67f1ca19132e641987528671f
David Zhang <v8os.com@gmail.com>
2022-05-03 21:26:49 +0800

    First commit

---
+++ token/token.go
@@ -0,0 +1,60 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN    = "="
+	PLUS      = "+"
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	BANG     = "!"
+	SLASH    = "/"
+	MINUS    = "-"
+	ASTERISK = "*"
+
+	GT = ">"
+	LT = "<"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	RETURN   = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"true":   TRUE,
+	"false":  FALSE,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
